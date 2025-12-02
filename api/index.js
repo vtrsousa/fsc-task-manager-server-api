ec842cc097d7754a34388ce58d0756fdfd711be4
joaosousa <joao.sousa@corebiz.ag>
2025-12-01 21:09:58 -0300

    feat: try server

--- api/index.js
+++ api/index.js
@@ -1,24 +1,20 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server"
+import path from "path"
 
-const fs = require("fs")
-const path = require("path");
-const filePath = path.join("db.json")
-const data = fs.readFileSync(filePath, "utf-8")
-const db = JSON.parse(data)
-const router = jsonServer.router(db)
+const server = jsonServer.create()
+const middlewares = jsonServer.defaults()
 
-const middlewares = jsonServer.defaults();
+const dbPath = path.join(process.cwd(), "db.json")
+const router = jsonServer.router(dbPath)
 
-server.use(middlewares);
+server.use(middlewares)
+
 server.use(
   jsonServer.rewriter({
-    "/api/*": "/$1",
+    "/api/*": "/$1"
   })
-);
+)
 
-server.use(router);
-server.listen(3000, () => {
-  console.log("JSON server running")
-})
+server.use(router)
 
-module.exports = server
+export default server
